a699c763a0a20d6ee111bda57fc51a0c8e393f7c
Carl-Ola Boketoft <calleboketoft@gmail.com>
2017-01-09 12:11:04 +0900

    update to new context solution

--- co-loopback-my-authorizations.js
+++ co-loopback-my-authorizations.js
@@ -1,78 +1,89 @@
-var debug = require('debug')('model:user');
+'use strict'
+
+var debug = require('debug')('model:user')
 module.exports = function (app, User) {
-    // Fetch authorizations for current user's id and roles
-    // TODO this could be modified to take a user id as param to
-    // see which authorizations any user has
-    User.myAuthorizations = function (cb) {
-        var loopback = require('loopback');
-        var RoleMapping = app.models.RoleMapping;
-        var Role = app.models.Role;
-        var ACL = app.models.ACL;
-        // https://github.com/strongloop/loopback/issues/569#issuecomment-62690674
-        // anywhere in the app
-        var ctx = loopback.getCurrentContext();
-        var currentUser = ctx && ctx.get('currentUser');
-        // When using MongoDB, the id is an object, but in the RoleMappings it is a string
-        var userId = currentUser.__data.id;
-        if (typeof userId === 'object') {
-            userId = userId.toString();
+  // Fetch authorizations for current user's id and roles
+  // see which authorizations any user has
+  User.myAuthorizations = (options, cb) => {
+    let RoleMapping = app.models.RoleMapping
+    let Role = app.models.Role
+    let ACL = app.models.ACL
+
+    let userId = options.accessToken.__data.userId
+
+    let authorizations = {
+      roles: [],
+      acls: []
+    }
+
+    debug('find({ where: { principalType: %s, principalId: %s }})', 'USER', userId)
+    RoleMapping.find({ where: { principalType: 'USER', principalId: userId } }, (err, roleMappings) => {
+      debug('found %s', roleMappings)
+      let rolesToFetch = roleMappings.map((roleMapping) => {
+        return {
+          id: roleMapping.roleId
         }
-        var authorizations = {
-            roles: [],
-            acls: []
-        };
-        debug('find({ where: { principalType: %s, principalId: %s }})', 'USER', userId);
-        RoleMapping.find({ where: { principalType: 'USER', principalId: userId } }, function (err, roleMappings) {
-            debug('found %s', roleMappings);
-            var rolesToFetch = roleMappings.map(function (roleMapping) {
-                return {
-                    id: roleMapping.roleId
-                };
-            });
-            Role.find({ where: { or: rolesToFetch } }, function (err, roles) {
-                var aclsToFetch = roles.map(function (role) {
-                    authorizations.roles.push(role.name);
-                    return {
-                        principalType: 'ROLE',
-                        principalId: role.name,
-                        permission: 'ALLOW'
-                    };
-                });
-                authorizations.roles.push('$everyone');
-                aclsToFetch.push({
-                    principalType: 'ROLE',
-                    principalId: '$everyone',
-                    permission: 'ALLOW'
-                });
-                authorizations.roles.push('$authenticated');
-                aclsToFetch.push({
-                    principalType: 'ROLE',
-                    principalId: '$authenticated',
-                    permission: 'ALLOW'
-                });
-                authorizations.roles.push('$owner');
-                aclsToFetch.push({
-                    principalType: 'ROLE',
-                    principalId: '$owner',
-                    permission: 'ALLOW'
-                });
-                ACL.find({ where: { or: aclsToFetch } }, function (err, acls) {
-                    authorizations.acls = acls.map(function (acl) {
-                        return {
-                            model: acl.model,
-                            property: acl.property,
-                            accessType: acl.accessType
-                        };
-                    });
-                    cb(null, authorizations);
-                });
-            });
-        });
-    };
-    User.remoteMethod('myAuthorizations', {
-        returns: { arg: 'authorizations', type: 'object' },
-        http: { verb: 'get' },
-        description: 'Find all ACLs with permission "ALLOW" and roles for the currently authenticated user.'
-    });
-};
-//# sourceMappingURL=co-loopback-my-authorizations.js.map
+      })
+
+      Role.find({ where: { or: rolesToFetch } }, (err, roles) => {
+        let aclsToFetch = roles.map((role) => {
+          authorizations.roles.push(role.name)
+          return {
+            principalType: 'ROLE',
+            principalId: role.name,
+            permission: 'ALLOW'
+          }
+        })
+
+        authorizations.roles.push('$everyone');
+        aclsToFetch.push({
+          principalType: 'ROLE',
+          principalId: '$everyone',
+          permission: 'ALLOW'
+        })
+
+        authorizations.roles.push('$authenticated')
+        aclsToFetch.push({
+          principalType: 'ROLE',
+          principalId: '$authenticated',
+          permission: 'ALLOW'
+        })
+
+        authorizations.roles.push('$owner')
+        aclsToFetch.push({
+          principalType: 'ROLE',
+          principalId: '$owner',
+          permission: 'ALLOW'
+        })
+
+        ACL.find({ where: { or: aclsToFetch } }, (err, acls) => {
+          authorizations.acls = acls.map((acl) => {
+            return {
+              model: acl.model,
+              property: acl.property,
+              accessType: acl.accessType
+            }
+          })
+          cb(null, authorizations)
+        })
+      })
+    })
+  }
+
+  User.remoteMethod('myAuthorizations', {
+    accepts: {
+      arg: 'options',
+      type: 'object',
+      http: 'optionsFromRequest'
+    },
+    returns: {
+      arg: 'authorizations',
+      type: 'object'
+    },
+    http: {
+      verb: 'get'
+    },
+    description: 'Find all ACLs with permission "ALLOW" and roles for the currently authenticated user.'
+  })
+}
+
